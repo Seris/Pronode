705c16a3f20bc16616f59e9ea46acd6ad608775c
Seris <seerriss@gmail.com>
2014-04-20 00:58:23 +0200

    On commente car non termine

--- http/server.js
+++ http/server.js
@@ -14,13 +14,13 @@ var app = Pronode.httpServer = express();
 app.engine('handlebars', Handlebars.compile);
 app.set('view engine', 'handlebars');
 
-fs.readdir(Pronode.main('http/router').replace('.js', ''), function(files){
+/* fs.readdir(Pronode.main('http/router').replace('.js', ''), function(files){
     var routes = require(Pronode.main('http/router/' + files));
     for(var r in routes){
         routes[r](app);
     }
-});
+}); */
 
-var port = Pronode.httpPort || process.env.NODE_PORT || 80;
+var port = Pronode.config.httpPort || process.env.NODE_PORT || 80;
 app.listen(port);
 console.info('Web Interface listen on ' + port);
